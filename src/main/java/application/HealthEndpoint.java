49ef3c2277a5be0381fb07f47427e6d3de22cdcf
takakiyo <t.takakiy@gmail.com>
2019-01-09 11:09:23 +0000

    add temp

--- src/main/java/application/HealthEndpoint.java
+++ src/main/java/application/HealthEndpoint.java
@@ -18,7 +18,7 @@ public class HealthEndpoint {
         return Response.status(503).entity("{\"status\":\"DOWN\"}").build();
       }
       */
-      return Response.ok("{\"status\":\"UP\"}").build();
+      return Response.ok("{\"status\":\"UP\", \"temp\":\"temp\" }").build();
     }
 
 }
